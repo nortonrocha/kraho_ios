6aeca2a7897ce602c26517c87160ef03d1a86f0c
Victor Barbosa <vicsoftsistemas@outlook.com>
2015-02-25 22:38:26 -0300

    Cube updated in 3D and it moved by touch direction

--- Krahô/MainGLKViewController.m
+++ Krahô/MainGLKViewController.m
@@ -8,12 +8,74 @@
 
 #import "MainGLKViewController.h"
 
+typedef struct {
+    float Position[3];
+    float Color[4];
+    float TextCoord[2];
+} Vertex;
+
+const Vertex Vertices[] = {
+    // Front
+    {{1, -1, 1}, {1, 0, 0, 1}, {1, 0}},
+    {{1, 1, 1}, {0, 1, 0, 1}, {1, 1}},
+    {{-1, 1, 1}, {0, 0, 1, 1}, {0, 1}},
+    {{-1, -1, 1}, {0, 0, 0, 1}, {0, 0}},
+    // Back
+    {{1, 1, -1}, {1, 0, 0, 1}, {0, 1}},
+    {{-1, -1, -1}, {0, 1, 0, 1}, {1, 0}},
+    {{1, -1, -1}, {0, 0, 1, 1}, {0, 0}},
+    {{-1, 1, -1}, {0, 0, 0, 1}, {1, 1}},
+    // Left
+    {{-1, -1, 1}, {1, 0, 0, 1}, {1, 0}},
+    {{-1, 1, 1}, {0, 1, 0, 1}, {1, 1}},
+    {{-1, 1, -1}, {0, 0, 1, 1}, {0, 1}},
+    {{-1, -1, -1}, {0, 0, 0, 1}, {0, 0}},
+    // Right
+    {{1, -1, -1}, {1, 0, 0, 1}, {1, 0}},
+    {{1, 1, -1}, {0, 1, 0, 1}, {1, 1}},
+    {{1, 1, 1}, {0, 0, 1, 1}, {0, 1}},
+    {{1, -1, 1}, {0, 0, 0, 1}, {0, 0}},
+    // Top
+    {{1, 1, 1}, {1, 0, 0, 1}, {1, 0}},
+    {{1, 1, -1}, {0, 1, 0, 1}, {1, 1}},
+    {{-1, 1, -1}, {0, 0, 1, 1}, {0, 1}},
+    {{-1, 1, 1}, {0, 0, 0, 1}, {0, 0}},
+    // Bottom
+    {{1, -1, -1}, {1, 0, 0, 1}, {1, 0}},
+    {{1, -1, 1}, {0, 1, 0, 1}, {1, 1}},
+    {{-1, -1, 1}, {0, 0, 1, 1}, {0, 1}},
+    {{-1, -1, -1}, {0, 0, 0, 1}, {0, 0}}
+};
+
+const GLubyte Indices[] = {
+    // Front
+    0, 1, 2,
+    2, 3, 0,
+    // Back
+    4, 6, 5,
+    4, 5, 7,
+    // Left
+    8, 9, 10,
+    10, 11, 8,
+    // Right
+    12, 13, 14,
+    14, 15, 12,
+    // Top
+    16, 17, 18,
+    18, 19, 16,
+    // Bottom
+    20, 21, 22,
+    22, 23, 20
+};
+
 @interface MainGLKViewController(){
-    float _curRed;
+    float _curRed, _rotation;
     BOOL _increasing;
     
     GLuint _vertexBuffer;
     GLuint _indexBuffer;
+    
+    GLKMatrix4 _rotMatrix;
 }
 
 @property (strong, nonatomic) EAGLContext *context;
@@ -38,6 +100,8 @@
     glGenBuffers(1, &_indexBuffer);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBuffer);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+    
+    _rotMatrix = GLKMatrix4Identity;
 }
 
 -(void) tearDownGL{
@@ -84,6 +148,18 @@
 -(void) glkView:(GLKView *)view drawInRect:(CGRect)rect{
     glClearColor(_curRed, 124, 205, 1);
     glClear(GL_COLOR_BUFFER_BIT);
+    
+    [self.effect prepareToDraw];
+    
+    glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBuffer);
+    
+    glEnableVertexAttribArray(GLKVertexAttribPosition);
+    glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid *) offsetof(Vertex, Position));
+    glEnableVertexAttribArray(GLKVertexAttribColor);
+    glVertexAttribPointer(GLKVertexAttribColor, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid *) offsetof(Vertex, Color));
+    
+    glDrawElements(GL_TRIANGLES, sizeof(Indices) / sizeof(Indices[0]), GL_UNSIGNED_BYTE, 0);
 }
 
 #pragma mark - GLKViewControllerDelegate
@@ -103,41 +179,33 @@
         _curRed = 0.0;
         _increasing = YES;
     }
+    
+    float aspect = fabsf(self.view.bounds.size.width / self.view.bounds.size.height);
+    GLKMatrix4 projectionMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(65.0f), aspect, 4.0f, 10.0f);
+    self.effect.transform.projectionMatrix = projectionMatrix;
+    
+    GLKMatrix4 modelViewMatrix = GLKMatrix4MakeTranslation(0.0f, 0.0f, -6.0f);
+    _rotation += 90 * self.timeSinceLastUpdate;
+    modelViewMatrix = GLKMatrix4Multiply(modelViewMatrix, _rotMatrix);
+    self.effect.transform.modelviewMatrix = modelViewMatrix;
 }
 
 -(void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
-    NSLog(@"Início...");
-    NSLog(@"   ");
-    NSLog(@"timeSinceLastUpdate: %f", self.timeSinceLastUpdate);
-    NSLog(@"timeSinceLastDraw: %f", self.timeSinceLastDraw);
-    NSLog(@"timeSinceFirstResume: %f", self.timeSinceFirstResume);
-    NSLog(@"timeSinceLastResume: %f", self.timeSinceLastResume);
-    
-    self.paused = !self.paused;
-    
-    NSLog(@"FIM...");
-    NSLog(@"   ");
-    NSLog(@"timeSinceLastUpdate: %f", self.timeSinceLastUpdate);
-    NSLog(@"timeSinceLastDraw: %f", self.timeSinceLastDraw);
-    NSLog(@"timeSinceFirstResume: %f", self.timeSinceFirstResume);
-    NSLog(@"timeSinceLastResume: %f", self.timeSinceLastResume);
+
 }
 
-typedef struct {
-    float Position[3];
-    float Color[4];
-} Vertex;
-
-const Vertex Vertices[] = {
-    {{1, -1, 0}, {1, 0, 0, 1}},
-    {{1, 1, 0}, {0, 1, 0, 1}},
-    {{-1, 1, 0}, {0, 0, 1, 1}},
-    {{-1, -1, 0}, {0, 0, 0, 1}}
-};
-
-const GLubyte Indices[] = {
-    0, 1, 2,
-    2, 3, 0
-};
-
+-(void) touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInView:self.view];
+    CGPoint lastLoc = [touch previousLocationInView:self.view];
+    CGPoint diff = CGPointMake(lastLoc.x - location.x, lastLoc.y - location.y);
+    
+    float rotX = -1 * GLKMathDegreesToRadians(diff.y / 2.0);
+    float rotY = -1 * GLKMathDegreesToRadians(diff.x / 2.0);
+    
+    GLKVector3 xAxis = GLKVector3Make(1, 0, 0);
+    _rotMatrix = GLKMatrix4Rotate(_rotMatrix, rotX, xAxis.x, xAxis.y, xAxis.z);
+    GLKVector3 yAxis = GLKVector3Make(0, 1, 0);
+    _rotMatrix = GLKMatrix4Rotate(_rotMatrix, rotY, yAxis.x, yAxis.y, yAxis.z);
+}
 @end
